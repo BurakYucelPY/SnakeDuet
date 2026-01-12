99a102817997b8f27735b6f86979af780064b298
karub360 <karub360@gmail.com>
2026-01-12 17:05:52 +0300

    Feat: Game Logic entegre edildi - Yılanlar el hareketiyle kontrol ediliyor

--- processor.py
+++ processor.py
@@ -1,32 +1,39 @@
 import cv2
 import mediapipe as mp
 from streamlit_webrtc import VideoTransformerBase
+from game import Yilan
 
 from mediapipe.python.solutions import hands as mp_hands
 from mediapipe.python.solutions import drawing_utils as mp_draw
 
 class VideoIsleyici(VideoTransformerBase):
     def __init__(self):
+        # MediaPipe Ayarları
         self.hands = mp_hands.Hands(
             max_num_hands=2,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
 
+        self.yilan_sol = Yilan(150, 150, (0, 255, 0))
+        self.yilan_sag = Yilan(450, 150, (0, 0, 255))
+
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
         
-        # Ayna Efekti
+        # Ayna Efekti ve Çizgiler
         img = cv2.flip(img, 1)
         h, w, _ = img.shape
         orta_nokta = w // 2
         
-        # Çizgi
         cv2.line(img, (orta_nokta, 0), (orta_nokta, h), (255, 255, 255), 2)
 
         # El Tespiti
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         sonuc = self.hands.process(img_rgb)
+
+        sol_komut = "DURUYOR"
+        sag_komut = "DURUYOR"
 
         if sonuc.multi_hand_landmarks:
             for el_noktalari in sonuc.multi_hand_landmarks:
@@ -35,14 +42,16 @@ class VideoIsleyici(VideoTransformerBase):
                 bilek = el_noktalari.landmark[0]
                 parmak_uc = el_noktalari.landmark[8]
 
-                bilek_x, bilek_y = int(bilek.x * w), int(bilek.y * h)
-                uc_x, uc_y = int(parmak_uc.x * w), int(parmak_uc.y * h)
+                bilek_x = int(bilek.x * w)
+                bilek_y = int(bilek.y * h)
+                uc_x = int(parmak_uc.x * w)
+                uc_y = int(parmak_uc.y * h)
 
                 fark_x = uc_x - bilek_x
                 fark_y = bilek_y - uc_y 
 
                 yon = "DURUYOR"
-                hassasiyet = 40 
+                hassasiyet = 30 
 
                 if abs(fark_x) > abs(fark_y):
                     if fark_x > hassasiyet: yon = "SAG"
@@ -52,8 +61,16 @@ class VideoIsleyici(VideoTransformerBase):
                     elif fark_y < -hassasiyet: yon = "ASAGI"
 
                 if bilek_x < orta_nokta:
-                    cv2.putText(img, f"SOL: {yon}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                    sol_komut = yon
+                    cv2.putText(img, f"SOL: {yon}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 else:
-                    cv2.putText(img, f"SAG: {yon}", (w - 350, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    sag_komut = yon
+                    cv2.putText(img, f"SAG: {yon}", (w - 200, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        self.yilan_sol.hareket_et(sol_komut, w, h)
+        self.yilan_sol.ciz(img, cv2)
+
+        self.yilan_sag.hareket_et(sag_komut, w, h)
+        self.yilan_sag.ciz(img, cv2)
 
         return img
